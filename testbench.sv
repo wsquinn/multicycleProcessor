0b21964b1c9870530e51ec1cc4334c5eb54cf3b6
Walker Quinn <35608215+wsquinn@users.noreply.github.com>
2018-12-31 15:57:06 -0800

    Add files via upload

---
+++ testbench.sv
@@ -0,0 +1,37 @@
+//testbench.sv
+
+module testbench();
+
+  logic        clk;
+  logic        reset;
+
+  logic [31:0] WriteData, DataAdr;
+  logic        MemWrite;
+
+  // instantiate device to be tested
+  top dut(clk, reset, WriteData, DataAdr, MemWrite);
+  
+  // initialize test
+  initial
+    begin
+      reset <= 1; # 17; reset <= 0;
+    end
+
+  // generate clock to sequence tests
+  always
+    begin
+      clk <= 1; # 5; clk <= 0; # 5;
+    end
+
+  // check results
+  always @(negedge clk)
+    begin
+      if(MemWrite) begin
+        if(DataAdr === 232) begin
+          $display("Simulation done");
+		  $display("WriteData %d", WriteData);
+          $stop;
+        end
+      end
+    end
+endmodule
